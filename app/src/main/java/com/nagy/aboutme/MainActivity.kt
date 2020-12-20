2d4afac2aeb032e1974ce7b532d8f7ff0ff535f2
ahmed nagy <ahmedsurviver@gmail.com>
2020-12-20 12:25:18 +0200

    Task Done !!

--- app/src/main/java/com/nagy/aboutme/MainActivity.kt
+++ app/src/main/java/com/nagy/aboutme/MainActivity.kt
@@ -2,9 +2,29 @@ package com.nagy.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener(this::addNickname)
+
+    }
+
+
+    private fun addNickname(view : View)
+    {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val NicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nickname_text.text = editText.text
+        editText.visibility = View.GONE
+        nickname_text.visibility = View.VISIBLE
     }
 }
